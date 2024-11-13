c719dafbe80ae669d8d91bed5f60dd20ea2d01f6
Lior Ifrach <lior.ifrach@post.runi.ac.il>
2024-11-14 01:26:18 +0200

    num added

--- NumWords.java
+++ NumWords.java
@@ -1,20 +1,13 @@
 // Prints a given number using a hundreds, tens, and units notation.
 public class NumWords {
-	public static void main(String args[]) {
-
-	int number = 0;
-	int ones = 0;
-	int hundreds = 0;
-	int tens = 0;
-
-	number = Integer.parseInt(args[0]);
-
-	ones = number % 10;
-	tens = ((number % 100) - ones ) / 10;
-	hundreds = (number / 100); 
-
-	System.out.println( hundreds + " hundreds " + tens + " tens, and " + ones + " ones ");
-	
-
-	}
+    public static void main(String[] args) {
+      
+        int num = Integer.parseInt(args[0]);
+        
+        int hundreds = num / 100;
+        int tens = (num % 100) / 10;
+        int ones = num % 10;
+      
+        System.out.println(hundreds + " hundreds, " + tens + " tens, and " + ones + " ones.");
+    }
 }
