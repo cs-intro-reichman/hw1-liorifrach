72f7f564ddbcbff30a59b24862b659f0d3e43b8e
Lior Ifrach <lior.ifrach@post.runi.ac.il>
2024-11-13 20:48:50 +0200

    num added

--- NumWords.java
+++ NumWords.java
@@ -2,10 +2,16 @@
 public class NumWords {
 	public static void main(String args[]) {
 
-	int number = Integer.parseInt(args[0]);
-	int ones = number % 10;
-	int hundreds = (number / 100); 
-	int tens = (((number % 10) - ones ) / 10);
+	int number = 0;
+	int ones = 0;
+	int hundreds = 0;
+	int tens = 0;
+
+	number = Integer.parseInt(args[0]);
+
+	ones = number % 10;
+	hundreds = (number / 100); 
+	tens = ((number % 100) - ones ) / 10;
 	
 
 	System.out.println( hundreds + " hundreds " + tens + " tens, and " + ones + " ones ");
