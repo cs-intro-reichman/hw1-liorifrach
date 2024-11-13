157f44d513477162d7b23d28e2da0d6081142357
Lior Ifrach <lior.ifrach@post.runi.ac.il>
2024-11-13 20:31:19 +0200

    time fixed

--- TimeFormat.java
+++ TimeFormat.java
@@ -3,18 +3,21 @@ public class TimeFormat {
 	public static void main(String[] args) {
 	
 		int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
-		// Does the same with the minutes part of the input.
 		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
-        // Replace this comment with the rest of your code
 		String period = "AM";
 		if (hours >= 12){
 			period = "PM";
 			if(hours > 12){
-				hours -= 12; // from 24 to 12 hours
+				hours = hours - 12; // from 24 to 12 hours
 
 			}
-		} else if(hours == 0){
-			hours = 12; // midnight
+		}
+		else if(hours == 0){
+			hours = 0; // midnight
+			period = "AM";
+		}
+		else{
+			period = "AM";
 		}
 		if (minutes < 10) {
 			System.out.println((hours + ":0" + minutes + " " + period));
