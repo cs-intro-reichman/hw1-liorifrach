50b4358e6c78334b35fe3115b7b00af2bb47ab7e
Lior Ifrach <lior.ifrach@post.runi.ac.il>
2024-11-14 01:12:20 +0200

    ascend added

--- Ascend.java
+++ Ascend.java
@@ -6,54 +6,19 @@ public class Ascend {
 		int num1 = (int)(Math.random()*lim + 1);
 		int num2 = (int)(Math.random()*lim + 1); 
 		int num3 = (int)(Math.random()*lim + 1); 
+
+		System.out.println(num1 + " " + num2 + " " + num3);
 		
-		int min = 0;
-		int middle = 0;
-		int max = 0; 
-		
-		// num1=min
-		if(num1 <= num2 && num1<= num3){
-			min = num1;
-			if(num2 <= num3){
-				middle = num2;
-				max = num3;
-			}
-			else{
-				middle = num3;
-				max = num2;
-			}
-		}
-		// num2=min
-		if(num2 <= num1 && num2 <= num3){
-			min = num2;
-			if(num1 <= num3)
-			{
-				middle = num1;
-				max = num3;
-			}
-			else{
-				middle = num3;
-				max = num1;
-			}
+		int min = Math.min(num1, Math.min(num2, num3));
+        int max = Math.max(num1, Math.max(num2, num3));
+        
+        // חישוב הערך האמצעי בעזרת חיסור
+        int middle = num1 + num2 + num3 - min - max;
 
-		}
-		// num3=min
-		if(num3 <= num1 && num3 <= num2){
-			min = num3;
-			if(num1 <= num2)
-			{
-				middle = num1;
-				max = num2;
-			}
-			else{
-				middle = num2;
-				max = num1;
-			}
+        // הדפסת המספרים בסדר עולה
+        System.out.println(min + " " + middle + " " + max);
+	
 
 		}
 
-
-		System.out.println(num1 +" "+ num2 +" "+ num1);
-		System.out.println(min +" "+ middle +" "+ max);
 	}
-}
