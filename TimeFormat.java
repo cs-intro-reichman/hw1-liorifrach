002abc140a152312ede14f5cd53824e3d36f31c4
Lior Ifrach <lior.ifrach@post.runi.ac.il>
2024-11-14 01:05:17 +0200

    time added

--- TimeFormat.java
+++ TimeFormat.java
@@ -1,23 +1,25 @@
-// Represents the hh:mm time format using an AM/PM format. 
 public class TimeFormat {
-	public static void main(String[] args) {
-	
-		int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
-		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
-		String period = "AM";
-
-		if (hours >= 12){
-			period = "PM";
-			if(hours > 12){
-				hours = hours - 12; // from 24 to 12 hours
-
-			}
-		}
-		
-			System.out.println(hours + ":");
-			System.out.printf("%02d", minutes);
-			System.out.println(" " + period);
-		
-
-		}
-	}
+    public static void main(String[] args) {
+        int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
+        int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
+        
+        String ampm = "AM";
+        
+        if (hours >= 12) {
+            ampm = "PM";  
+            if (hours > 12) {
+                hours = hours - 12;  
+            }
+        }
+        
+        if (hours == 0) {
+            hours = 12;
+        }
+        
+        System.out.print(hours + ":");  
+       
+        System.out.printf("%02d", minutes); 
+    
+        System.out.println(" " + ampm);
+    }
+}
