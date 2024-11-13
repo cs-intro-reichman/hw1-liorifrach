aae56b4f52b583dcd232a327fc256a60ed4ebb4c
Lior Ifrach <liorifrach@h-MacBook-Air-sl-Lior.local>
2024-11-13 11:52:52 +0200

    numwords added

--- FVCalc.java
+++ FVCalc.java
@@ -1,6 +1,26 @@
 // Computes the future value of a saving investment.
 public class FVCalc {
-	public static void main(String[] args){
-		// Replace this comment with your code
+	public FVCalc() {
 	}
+		public static void main(String[] var0){
+
+			int var1 = Integer.parseInt(var0[0]);
+			int var2 = Integer.parseInt(var0[0]);
+			int var3 = Integer.parseInt(var0[0]);
+			int finelamount=var1; 
+			// calculatint the future amount by incrising var1 every year
+			for(int i=0; i<=var3; i++)
+			{
+				finelamount=finelamount + (finelamount*(var3/100));
+			}
+			System.out.println("after" + var2 + "years," + var1+"$, saves at"+var3+"% will yeild "+finelamount+"$");
+
+		// Replace this comment with your code
+		
+	
+		int yield = 0; 
+	}
+	
+
+	
 }
