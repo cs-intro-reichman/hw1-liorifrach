52da828cf2afb23776d7edd58e63888d483415da
Lior Ifrach <lior.ifrach@post.runi.ac.il>
2024-11-14 00:46:53 +0200

    fvc added

--- FVCalc.java
+++ FVCalc.java
@@ -5,13 +5,12 @@ public class FVCalc {
 		public static void main(String[] var0){
 
 			int currect = Integer.parseInt(var0[0]);
-
 			double rate = Double.parseDouble(var0[1]);
 			int length = Integer.parseInt(var0[2]);
 		
 			double future = 0;
 			
-			future = currect * Math.pow(1 + rate / 100 , length);
+			future = currect * Math.pow(1 + rate / 100, length);
 			
 			System.out.println("After " + length + " years, $" + currect + " saved at " + rate + "% will yield $" + (int)future);
 		
